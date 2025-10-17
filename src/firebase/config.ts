6def32dff4f1611444e182d2ea68ba67dd4410ee
22R11A05U7 A K DINESH <22r11a05u7@gcet.edu.in>
2025-10-17 07:44:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3387703828-fe724",
+  "appId": "1:884805605913:web:8ff1a5b3aab2ab514a6888",
+  "apiKey": "AIzaSyBxey6lZyhQ8hpF5VmTg4XWJsoVNAK6PSE",
+  "authDomain": "studio-3387703828-fe724.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "884805605913"
+};
